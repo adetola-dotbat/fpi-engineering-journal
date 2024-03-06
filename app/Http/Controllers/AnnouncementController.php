7f868f4e5becffdc32a90f333fe8159b7983cae4
Adewole Noah <adewolenoah4@gmail.com>
2024-03-06 07:03:07 +0100

    announcement controller

--- app/Http/Controllers/AnnouncementController.php
+++ app/Http/Controllers/AnnouncementController.php
@@ -2,9 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AnnouncementRequest;
+use App\Models\Announcement;
+use App\Services\AnnouncementService;
 use Illuminate\Http\Request;
 
 class AnnouncementController extends Controller
 {
-    //
+    public function __construct(protected AnnouncementService $announcementService)
+    {
+    }
+    public function index()
+    {
+        $data = [
+            'announcements' => $this->announcementService->all(),
+        ];
+        return view('user.pages.index', $data);
+    }
+
+    public function announcement()
+    {
+        $data = [
+            'title_page' => 'Announcement',
+            'announcements' => $this->announcementService->all(),
+        ];
+        return view('admin.pages.index', $data);
+    }
+    public function edit($announcement)
+    {
+        $data = [
+            'title_page' => 'Edit Announcement',
+            'announcements' => $this->announcementService->all(),
+            'announcement' => $this->announcementService->getAnnouncementById($announcement),
+        ];
+        return view('admin.pages.edit', $data);
+    }
+    public function update(AnnouncementRequest $request, $announcement)
+    {
+        $this->announcementService->update($announcement, $request->validated());
+        return redirect()->back();
+    }
+    public function destroy($announcement)
+    {
+        $this->announcementService->delete($announcement);
+        return redirect()->bank();
+    }
 }
